09768aa542bc97e2ed4eee01d59a608add0309de
fihri aziz <fihriaziz@gmail.com>
2022-05-12 10:29:07 +0700

    add express js

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-const http = require("http");
-const testModule = require("./testmodule");
-const { faker } = require("@faker-js/faker");
-
-const randomName = faker.name.findName();
-
-const hostname = "127.0.0.1";
+const express = require("express");
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  const url = req.url;
-  if (url === "/users") {
-    res.end("Hello users");
-  } else if (url === "/category") {
-    res.end("Mobil");
-  } else {
-    res.end(randomName);
-  }
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
